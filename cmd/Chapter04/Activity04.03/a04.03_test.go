5104e8322009c53430db42ed2d9fd8762ad4ae83
ibiscum <ulf.luecking@gmail.com>
2024-02-12 22:50:47 +0100

    Rename.

--- cmd/Chapter04/Activity04.03/a04.03_test.go
+++ cmd/Chapter04/Activity04.03/a04.03_test.go
@@ -5,18 +5,10 @@ import (
 )
 
 func TestGetLocales(t *testing.T) {
-<<<<<<< HEAD:cmd/Chapter04/Activity04.03/a04.03_test.go
 	// if exists := getLocales(); exists {
 	// 	t.Fail()
 	// }
 	// if exists := localeExists(1); !exists || name != "Sue" {
-=======
-	// if _, exists := getName(""); exists {
-	// 	t.Fail()
-	// }
-	// if name, exists := getName("305"); !exists || name != "Sue" {
-	// 	t.Fail()
-	// }
 }
 
 func TestLocaleExists(t *testing.T) {
@@ -24,7 +16,6 @@ func TestLocaleExists(t *testing.T) {
 	// 	t.Fail()
 	// }
 	// if name, exists := getName("305"); !exists || name != "Sue" {
->>>>>>> 95dff5c (Rename.):Chapter04/Activity04.03/a04.03_test.go
 	// 	t.Fail()
 	// }
 }
