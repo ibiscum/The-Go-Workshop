8efac8de89b9f7e641bda84e6f3e8d02288dbf09
ibiscum <ulf.luecking@gmail.com>
2024-02-12 22:37:43 +0100

    Rename.

--- cmd/Chapter01/Exercise01.01/e01.01.go
+++ cmd/Chapter01/Exercise01.01/e01.01.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
-	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 	r := rand.Intn(5) + 1
 	stars := strings.Repeat("*", r)
 	fmt.Println(stars)
--- cmd/Chapter04/Activity04.06/a04.06_test.go
+++ cmd/Chapter04/Activity04.06/a04.06_test.go
@@ -4,13 +4,7 @@ import (
 	"testing"
 )
 
-func TestGetLocales(t *testing.T) {
-<<<<<<< HEAD:cmd/Chapter04/Activity04.03/a04.03_test.go
-	// if exists := getLocales(); exists {
-	// 	t.Fail()
-	// }
-	// if exists := localeExists(1); !exists || name != "Sue" {
-=======
+func TestGetData(t *testing.T) {
 	// if _, exists := getName(""); exists {
 	// 	t.Fail()
 	// }
@@ -19,12 +13,11 @@ func TestGetLocales(t *testing.T) {
 	// }
 }
 
-func TestLocaleExists(t *testing.T) {
+func TestGetTypeName(t *testing.T) {
 	// if _, exists := getName(""); exists {
 	// 	t.Fail()
 	// }
 	// if name, exists := getName("305"); !exists || name != "Sue" {
->>>>>>> 95dff5c (Rename.):Chapter04/Activity04.03/a04.03_test.go
 	// 	t.Fail()
 	// }
 }
