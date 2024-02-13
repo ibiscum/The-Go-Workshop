06f25a90903a6bf15ff7d20a8f40e04699063a1a
ibiscum <ulf.luecking@gmail.com>
2024-02-14 00:03:41 +0100

    Tests.

---
+++ cmd/Chapter14/Activity14.01/solution/solution_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+// TestGetDataAndParseResponse requires the server to be running to succeed
+func TestGetDataAndParseResponse(t *testing.T) {
+	if os.Getenv("TEST_NO_CI") != "" {
+		t.Skip("Skipping not finished test")
+	}
+	electricCount, boogalooCount := getDataAndParseResponse()
+	if electricCount < 1 {
+		t.Errorf("expected more than one name 'Electric', recieved: %d", electricCount)
+	}
+	if boogalooCount < 1 {
+		t.Errorf("expected more than one name 'Boogaloo', recieved: %d", boogalooCount)
+	}
+}
