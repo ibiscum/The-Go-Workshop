f85a30cd788af0b1365e76551a7333aeb0569b7c
ibiscum <ulf.luecking@gmail.com>
2024-02-16 02:11:34 +0100

    Linting.

--- cmd/Chapter06/Exercise06.03/e06.03.go
+++ cmd/Chapter06/Exercise06.03/e06.03.go
@@ -15,10 +15,14 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(pay)
+
 	pay, err = payDay(80, 5)
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(pay)
+
 	pay, err = payDay(80, 50)
 	if err != nil {
 		fmt.Println(err)
@@ -30,14 +34,17 @@ func payDay(hoursWorked, hourlyRate int) (int, error) {
 	if hourlyRate < 10 || hourlyRate > 75 {
 		return 0, ErrHourlyRate
 	}
+
 	if hoursWorked < 0 || hoursWorked > 80 {
 		return 0, ErrHoursWorked
 	}
+
 	if hoursWorked > 40 {
 		hoursOver := hoursWorked - 40
 		overTime := hoursOver * 2
 		regularPay := hoursWorked * hourlyRate
 		return regularPay + overTime, nil
 	}
+
 	return hoursWorked * hourlyRate, nil
 }
