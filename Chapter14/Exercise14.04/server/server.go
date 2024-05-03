35d3087edce9dcdfc94e198783268a2a7f3868af
Ulf Luecking <ulf.luecking@gmail.com>
2024-05-04 01:35:19 +0200

    Security.

--- Chapter14/Exercise14.04/server/server.go
+++ Chapter14/Exercise14.04/server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"io"
 	"log"
 	"net/http"
@@ -27,7 +28,7 @@ func (srv server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	_, err = w.Write([]byte(fmt.Sprintf("%s Uploaded!", uploadedFileHeader.Filename)))
+	_, err = w.Write([]byte(fmt.Sprintf("%s Uploaded!", html.EscapeString(uploadedFileHeader.Filename))))
 	if err != nil {
 		log.Fatal(err)
 	}
